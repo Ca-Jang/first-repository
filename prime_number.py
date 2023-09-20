d97b12b80c6741592c0f8075764251ee5e92d619
Ca-Jang <caleb1117@naver.com>
2023-09-20 14:46:19 +0900

    Update prime_number.py

--- prime_number.py
+++ prime_number.py
@@ -6,4 +6,3 @@ def is_prime_number(x):
             if x % num == 0:
                 print("The number {num1} is not a prime number.".format(num1=x))
         print("The number {num1} is a prime number.".format(num1=x))
-
